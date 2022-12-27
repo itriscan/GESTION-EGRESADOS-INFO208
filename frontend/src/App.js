8ee0e835944837642beea9ce40dc8b636e43e870
itriscan <cristian.quilodran@outlook.com>
2022-12-27 20:13:49 -0300

    se inicializa frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Formulario from './component/Formulario';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Esto es un texto</h1>
+      </header>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ frontend/src/component/Formulario.js
@@ -0,0 +1,8 @@
+
+function Formulario(props) {
+    return (
+        <h1>Hello, {props.name}</h1>
+    )
+}
+
+export default Formulario;
